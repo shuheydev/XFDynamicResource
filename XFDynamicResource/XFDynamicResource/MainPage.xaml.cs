bdeeb0818e7e2a3b0220397a8e81ca41ff89aab6
Shuhey <shuhey.dev@gmail.com>
2020-05-13 15:09:23 +0900

    Add project files.

---
+++ XFDynamicResource/XFDynamicResource/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFDynamicResource
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        static bool IsChanged = false;
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (IsChanged)
+            {
+                App.Current.Resources["PrimaryColor"] = Color.Red;
+                App.Current.Resources["PrimaryColor2"] = "#FF0000";
+            }
+            else
+            {
+                App.Current.Resources["PrimaryColor"] = Color.Green;
+                App.Current.Resources["PrimaryColor2"] = "#00FF00";
+            }
+            IsChanged = !IsChanged;
+        }
+    }
+}
